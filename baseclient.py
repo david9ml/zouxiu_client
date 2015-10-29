6a10aa9405e1bb760fb613f54603d80bc86d938f
yanchao727 <yvogue@y01.ydc.yvogue.com>
2015-10-29 16:53:59 +0800

    init zouxiu_client

---
+++ baseclient.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from secret_file import *
+from time import gmtime, strftime
+from datetime import timedelta, datetime
+import requests
+import json
+
+class Baseclient(object):
+    def __init__(self):
+        self.app_key = APP_KEY
+        self.app_secret = APP_SECRET
+        self.params = {}
+        self.url = API_URL
+        self.uid = UID
+        self.timestamp = ''
+        self.sign = ''
+        self.path = ''
+        self.data = ''
+        self.headers = {'content-type': 'application/json'}
+        self.params = {"uid": self.uid, "charset": "utf-8", "format": "json", "language": "zh_CN"}
+        self.get_token()
+
+    def get_token(self):
+        self.set_path('/api/user/token')
+        request_data = {"username":self.app_key, "password": self.app_secret, "timestamp": datetime.now().strftime("%Y%m%d%H%M%S")}
+        response = requests.request('PUT', self.url+self.path, json=request_data, headers=self.headers, params=self.params)
+        content = response.content
+        response_dict = json.loads(content)
+        self.params['token'] = response_dict['data']
+        print(response_dict['data'])
+        return response_dict['data']
+
+    def print_secrets(self):
+        print(self.app_key)
+        print(self.app_secret)
+
+    def reset(self):
+        import gc
+        gc.collect()
+
+    def req_put(self):
+        response = requests.request('PUT', self.url+self.path, json=self.data, params=self.params)
+        return response
+
+    def req_post(self):
+        print(self.data)
+        print(self.params)
+        response = requests.request('POST', self.url+self.path, json=self.data, params=self.params)
+        return response
+
+    def set_path(self, path_value):
+        self.path = path_value
+        return True
+
+    def set_data(self, data):
+        self.data = data
+        return True
---
+++ zouxiu_client.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 *-
+from baseclient import *
+
+class Zouxiu_client(Baseclient):
+
+    def getitem(self, data):
+        self.set_path('/api/supplier/item/getitem')
+        self.set_data(data)
+        return self.req_put().content
+
+    def product(self, data):
+        self.set_path('/api/supplier/product')
+        self.set_data(data)
+        return self.req_post().content
+
