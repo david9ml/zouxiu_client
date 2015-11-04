1fdaed82335397e84314df58bc9455aeacb5e1e0
yanchao727 <yvogue@y01.ydc.yvogue.com>
2015-11-04 16:26:43 +0800

    upload product

--- start.py
+++ start.py
@@ -4,14 +4,25 @@ import time
 import json
 
 zouxiu_client = Zouxiu_client()
-print(zouxiu_client.product(data={"productId": "0000000".encode("utf-8"),
-                                  "productName": "test0000000".encode("utf-8"),
-                                  "brandNameZhs": "Prada".encode("utf-8"),
-                                  "productDesc": "Prada".encode("utf-8"),
-                                  "catName": "fengyi".encode("utf-8")}))
+request_data = [
+                {"productDetailUrl": "http://www.xiu.com",
+                 "xopSupplierItems": [
+                 {"productId": "0000000",
+                 "productName": "test0000003",
+                 "brandNameZhs": "Prada",
+                 "productDesc": "Prada",
+                 "itemId": "test0000003",
+                 "stock":1,
+                 "supplyPrice":23.6,
+                 "catName": "fengyi"}
+                 ],
+                 "brandNameZhs":"普拉达",
+                 "productName":"test0000003",
+                 "productId":"test0000003"
+                }
+               ]
+print(zouxiu_client.product(data=request_data))
 #print(zouxiu_client.getitem(data={}))
-
-
 
 
 '''
@@ -26,8 +37,6 @@ while True:
     print(content)
     shangpin_client.reset()
     time.sleep(1)
-'''
-'''
 # 3.2 商品批量分页查询
 shangpin_client.set_path('/commodity/findinfobypage')
 request_data = {"PageIndex":"1","PageSize":"100","endTime":"","startTime":""}
@@ -35,8 +44,6 @@ shangpin_client.set_request_data(request_data)
 response = shangpin_client.req_post()
 content = response.content
 print(content)
-'''
-'''
 # 3.3 品类信息查询(未完成)
 shangpin_client.set_path('/commodity/findcategorybypage')
 request_data = {"PageIndex":"1","PageSize":"200"}
@@ -44,8 +51,6 @@ shangpin_client.set_request_data(request_data)
 response = shangpin_client.req_post()
 content = response.content
 print(content)
-'''
-'''
 # 3.4 品类下销售属性查询
 shangpin_client.set_path('/commodity/findproductattr')
 request_data = {"CategoryNo": "A01B01C01D02"}
@@ -53,8 +58,6 @@ shangpin_client.set_request_data(request_data)
 response = shangpin_client.req_post()
 content = response.content
 print(content)
-'''
-'''
 #3.7 基础色系查询（未完成）
 shangpin_client.set_path('/commodity/findcolor')
 request_data = {}
@@ -62,8 +65,6 @@ shangpin_client.set_request_data(request_data)
 response = shangpin_client.req_post()
 content = response.content
 print(content)
-'''
-'''
 #3.8 品牌信息查询（未完成）
 shangpin_client.set_path('/commodity/findbrandbypage')
 request_data = {"PageIndex":"1","PageSize":"200"}
@@ -71,8 +72,6 @@ shangpin_client.set_request_data(request_data)
 response = shangpin_client.req_post()
 content = response.content
 print(content)
-'''
-'''
 #3.9 地区信息查询（未完成）
 shangpin_client.set_path('/commodity/findareabypage')
 request_data = {"PageIndex":"1","PageSize":"200"}
@@ -80,16 +79,12 @@ shangpin_client.set_request_data(request_data)
 response = shangpin_client.req_post()
 content = response.content
 print(content)
-'''
-'''
 shangpin_client.set_path('/base/currency')
 request_data = {}
 shangpin_client.set_request_data(request_data)
 response = shangpin_client.req_post()
 content = response.content
 print(content)
-'''
-'''
 # 1.1. 供货价查询
 shangpin_client.set_path('/supply/findinfo')
 request_data = {"Starttime":"","Endtime":"", "SkuNos":"30003221001", "PriceStatus":""}
@@ -97,8 +92,6 @@ shangpin_client.set_request_data(request_data)
 response = shangpin_client.req_post()
 content = response.content
 print(content)
-'''
-'''
 # 1.2. 供货价更新 未完成
 shangpin_client.set_path('/supply/updateprice')
 request_data = {"SkuNo":"30003221001","SupplyPrice":"100.00","MarkePrice":"900.00"}
@@ -106,8 +99,6 @@ shangpin_client.set_request_data(request_data)
 response = shangpin_client.req_post()
 content = response.content
 print(content)
-'''
-'''
 # 2.1 库存查询
 shangpin_client.set_path('/stock/findinfo')
 request_data = ["30003221001", "30002856001"]
@@ -116,8 +107,6 @@ shangpin_client.set_request_data(request_data)
 response = shangpin_client.req_post()
 content = response.content
 print(content)
-'''
-'''
 # 2.2 库存更新
 shangpin_client.set_path('/stock/update')
 request_data = {"SkuNo":"30003221001", "InventoryQuantity":100 }
@@ -125,8 +114,6 @@ shangpin_client.set_request_data(request_data)
 response = shangpin_client.req_post()
 content = response.content
 print(content)
-'''
-'''
 # 5.1 采购单分页查询
 shangpin_client.set_path('/purchase/findporderbypage')
 request_data = {"PageIndex":"1","PageSize":"20","UpdateTimeBegin":"2015-07-16 14:00:00","UpdateTimeEnd":"2015-07-24 23:59:59"}
@@ -134,8 +121,6 @@ shangpin_client.set_request_data(request_data)
 response = shangpin_client.req_post()
 content = response.content
 print(content)
-'''
-'''
 # 6.1 发货单分页查询
 shangpin_client.set_path('/purchase/finddorderbypage')
 request_data = {"PageIndex":"1","PageSize":"20","UpdateTimeBegin":"2014-01-16 14:00:00","UpdateTimeEnd":"2015-01-17 14:00:00"}
@@ -143,8 +128,6 @@ shangpin_client.set_request_data(request_data)
 response = shangpin_client.req_post()
 content = response.content
 print(content)
-'''
-'''
 # 7.1 返货单分页查询
 shangpin_client.set_path('/purchase/findrorderbypage')
 request_data = {"PageIndex":"1","PageSize":"20","UpdateTimeBegin":"2014-01-16 14:00:00","UpdateTimeEnd":"2015-01-17 14:00:00"}
